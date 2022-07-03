bc400a00c0fe00ac437f05b6185caebb7bc7e812
Sai bharath kumar <48505814+saibharath48@users.noreply.github.com>
2022-07-03 18:25:11 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+setInterval(() =>{
+    var d = new Date();
+    htime = d.getHours();
+    mtime = d.getMinutes();
+    stime = d.getSeconds();
+
+    if(htime>=12)
+    {
+        htime=htime-12;
+    }
+ 
+    hrotation = 30*htime + mtime/2;
+    mrotation = 6*mtime;
+    srotation = 6*stime;
+
+    hours.style.transform = `rotate(${hrotation}deg)`;
+    minutes.style.transform = `rotate(${mrotation}deg)`;
+    seconds.style.transform = `rotate(${srotation}deg)`;
+},1000);
